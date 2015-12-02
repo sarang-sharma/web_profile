6c982fe2aabeb3e661fe07a897da706820a2f907
sarang-sharma <sarang.sharma14@gmail.com>
2015-12-02 21:08:01 +0530

    Added post CRUD ability

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+	
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	def index
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new post_params
+
+		if @post.save
+			redirect_to @post, notice: "Hellz yeah Sarang! Your article was successfully saved"
+		else
+			render 'new', notice: "Ohh no! I was unable to save your post"
+		end
+	end
+
+	def show
+		
+	end
+
+	def edit
+	
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to posts_path
+	end
+
+	def update
+		if @post.update post_params
+			redirect_to @post, notice: "Your article was successfully updated !"
+		else
+			render 'edit'
+		end
+	end
+
+	private
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
+
+end
+
